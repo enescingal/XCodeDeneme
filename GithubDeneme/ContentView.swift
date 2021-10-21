d49d31fe683e86c0fbd53cfd98578e09c22805cb
enescingal <ecingal10@gmail.com>
2021-10-21 12:35:44 +0300

    Initial Commit

---
+++ GithubDeneme/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GithubDeneme
+//
+//  Created by Enes on 21.10.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GithubDeneme/GithubDenemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubDenemeApp.swift
+//  GithubDeneme
+//
+//  Created by Enes on 21.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct GithubDenemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
